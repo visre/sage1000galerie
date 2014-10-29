1b6576cdf08dc55d139a476f53fe70769555bbd9
viracsreng <srengvirac@hotmail.com>
2014-10-29 11:42:01 +0100

    initial commit

--- app.js
+++ app.js
@@ -224,7 +224,7 @@ app.post('/gallery/addProduct', function(req, res){
 
 
 Init();
-var server = app.listen(81, function() {
+var server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
